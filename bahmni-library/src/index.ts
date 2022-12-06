8a34f2dbd5803cab2e45343b7bc069739e81dacf
anjali-arakka <anjali.arakka@thoughtworks.com>
2022-12-06 15:35:08 +0530

    Add. index.ts

--- bahmni-library/src/index.ts
+++ bahmni-library/src/index.ts
@@ -1,5 +1,35 @@
 export * from './code'
+import { off } from 'process'
 import {performDOMOperations} from './bahmni'
+import { getLocationUuid, getPatientUuid } from './bahmni-patient-details'
 
 if(document.getElementById("sa-app"))
 { performDOMOperations()}
+
+
+const createScript = () => {
+  const script = document.createElement('script')
+  script.src='dist/speech_assistant/speech_assistant.bundle.js';
+  script.setAttribute('id', 'sa-bahmni-distribution')
+  script.setAttribute('patientUuid', getPatientUuid())
+  script.setAttribute('locationUuid', getLocationUuid())
+  document.body.appendChild(script)
+  console.log('inside create script -------')
+}
+
+let patientId=getPatientUuid()
+let locationId=getLocationUuid()
+console.log(patientId)
+console.log(locationId);
+window.addEventListener('hashchange', () => {
+  patientId=getPatientUuid()
+  locationId=getLocationUuid()
+  console.log("inside event listener")
+  console.log(patientId)
+  console.log(locationId)
+    // script.setAttribute('patientUuid', getPatientUuid())
+
+})
+createScript()
+
+  
